9612f38198fa3987738b99bf911018e4f758a7ed
matthew goodwin <matthewjgoodwin@gmail.com>
2020-03-17 02:17:12 +0900

    changes to app/javascript/packs/applicatio.js <br> ADDED app/javascript/plugins/init_mapbox.js <br> There is a bug in the Mapbox(or webpacker) software, that does not recognize the API key in the .env file; thus I create a new API key; this did not solve the issue; THIS IS THE EXACT ERROR::Uncaught Error: An API access token is required to use Mapbox GL. <br> ALSO there are several - Uncaught ReferenceError: _typeof is not defined- errors in the console.. Mapbox does not function. After research, it seems that there is an error/bug that is resolved by adding a line of CODE in the config/webpack/environment.js file(REFERENCE THE COMMIT COMMENTS BELOW FOR THE CODE.. )I'm commiting the JS before adding this CODE.. NOTE: node/modules are untracked, so any changes to that file are permanent and cant be rolled back.. there are not records outside the this local drive

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,6 @@
+// app/javascript/packs/application.js
 import 'bootstrap';
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
