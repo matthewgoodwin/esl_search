c98780229f6441e456963e3ca818ff84c71c974e
matthew goodwin <matthewjgoodwin@gmail.com>
2020-04-02 02:04:50 +0900

    I just deleted the longitude and latitude migration files, and as expected, I still have longitude and latitude columns in my schema.. I have checked every migration file and have found not migration for longitude and latitude.. not sure why I have them

--- db/migrate/20200311055534_remove_coord_from_consultations.rb
+++
@@ -1,6 +0,0 @@
-class RemoveCoordFromConsultations < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :consultations, :longitude, :float
-    remove_column :consultations, :latitude, :float
-  end
-end
--- db/migrate/20200311060050_add_coordslonglat_to_consultations.rb
+++
@@ -1,6 +0,0 @@
-class AddCoordslonglatToConsultations < ActiveRecord::Migration[5.0]
-  def change
-    add_column :consultations, :longitude, :float
-    add_column :consultations, :latitude, :float
-  end
-end
