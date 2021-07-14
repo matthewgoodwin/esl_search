25c46ce98c1d99e2bdff0cd221bc77f5d61af825
matthew goodwin <matthewjgoodwin@gmail.com>
2021-07-14 21:46:48 +0900

    Instance Methods for USERS<br> ICON UPDATE: all user icons updated; Still need to clean up orphan pages: REVIEW, PRIVATE MESSAGE, MESSAGE, etc. need to be updated to the current layout using content-grids; STILL need to update the average_star ratings; created a new file for the _index_data.

--- app/models/privatemessage.rb
+++ app/models/privatemessage.rb
@@ -3,4 +3,8 @@ class Privatemessage < ApplicationRecord
 
   validates :subject, :pm, presence: true
   validates :pm, length: { maximum: 350, message: "your private message should be shorter than 349 characters!"}
+
+  def find_user(element)
+    User.find(element.sender_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,11 +17,15 @@ class User < ApplicationRecord
 
          validates :fname, :username, :user_native_lang, :email, :city, presence: true
          validates :fname, uniqueness: {scope: :email, message: "already a memeber? please log in!"}
+
+  def first_in(element) #element if @consultations instance from _index.html
+    element.fname.slice(0)
+  end
   private
     # def anyeong
     #   UserMailer.welcome(self).deliver_now
     # end
     def set_color
-      self.color = ["#63b4d1","6da7d3", "#7699d4","#816ec4","#8658bc","#8b42b3","#602278"].sample
+      self.color = ["#7AD8E5", "#63b4d1", "6da7d3", "#7699d4", "#816ec4", "#8658bc", "#602278", "#34023C"].sample
     end
 end
