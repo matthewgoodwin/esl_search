e2d6af37ee68df0f4486b0f346c644c4dc906ef5
matthew goodwin <matthewjgoodwin@gmail.com>
2021-09-17 22:32:21 +0900

    UPDATE JOBPOSTS: users can now create job Posts; this included the permission for policy and redirect for errors and valid job posts. this will all need to be cleaned later; but for now it functions properly and redirects users to the #show page

--- app/controllers/job_posts_controller.rb
+++ app/controllers/job_posts_controller.rb
@@ -1,7 +1,9 @@
 class JobPostsController < ApplicationController
   # created rails g JobPosts Sept 15, 2021
-  skip_before_action :authenticate_user!, only:[:index, :show]
-  skip_after_action :verify_authorized, only: [:index, :show]
+  skip_before_action :authenticate_user!, only:[:index, :show, :new, :create]
+  skip_after_action :verify_authorized, only: [:index, :show, :new, :create]
+  before_action :job_post_params, only:[:create]
+  before_action :find_job, only: [:show, :edit, :update, :destroy]
   def index
     @job_posts = policy_scope(JobPost)
   end
@@ -9,9 +11,20 @@ class JobPostsController < ApplicationController
   end
 
   def new
+    @job_post = JobPost.new
+    # authorize @job_post
   end
 
   def create
+    @job_post = JobPost.new(job_post_params)
+    @job_post.user = current_user
+    # authorize @job_post
+    if @job_post.valid?
+      @job_post.save
+      redirect_to job_post_path(@job_post)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -23,4 +36,11 @@ class JobPostsController < ApplicationController
   def destroy
   end
   private
+  def job_post_params
+    params.require(:job_post).permit(:job_lang, :job_type, :job_demo, :job_period, :job_env, :job_expert, :job_desc, :job_fee_low, :job_fee_high)
+  end
+  def find_job
+    @job_post = JobPost.find(params[:id])
+    authorize @job_post
+  end
 end
--- app/policies/job_post_policy.rb
+++ app/policies/job_post_policy.rb
@@ -7,8 +7,29 @@ class JobPostPolicy < ApplicationPolicy
 def index?
   return true
 end
-def show
+def show?
   return true
 end
-
+def new?
+  return true
+end
+def create?
+  return true
+end
+def edit?
+  owner?
+end
+def update?
+  owner?
+end
+def destroy?
+  owner?
+end
+private
+  def owner?
+    record.user == user
+      # ^ same as: `record.user == user ? true : false`
+      # ^^ @consultation.user == current_user
+      # ^^^ from the `find_consultation` before_action on the consultation#controller
+  end
 end
