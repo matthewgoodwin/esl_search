6d669148f3d2dae0933d6aa525fa1229bee68a30
matthew goodwin <matthewjgoodwin@gmail.com>
2021-10-03 02:34:59 +0900

    Review update to validations; I still need to update validations for the other review categories

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,8 +3,8 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates :review_title, :punct, :prep, :org, :know, :comm, :adapt, :respond, :succ, presence: true
-  # validates :star, numericality: { only_integer: true }
-  # validates :star, inclusion: { in: (1..5), message: "Your rating should be between 1 and 5!" }
+  validates :star, numericality: { only_integer: true }
+  validates :star, inclusion: { in: (1..5), message: "Your rating should be between 1 and 5!" }
   # validates :user, uniqueness: { scope: :email, message: "you can only create one review!"}
   private
   def self.ravg_star(element)
