20551e0cacbb22ef604ad5994f579509be72c4a6
matthew goodwin <matthewjgoodwin@gmail.com>
2021-10-27 18:36:52 +0900

    JobPost Layout and ContentGrid update on the JobPost#Show page; This still needs cleanup<br> Added a new method for the expert<br>

--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -2,4 +2,19 @@ class JobPost < ApplicationRecord
   belongs_to :user
 
   validates :job_title, :job_lang, :job_type, :job_period, :job_expert, :job_desc, presence: true
-end
+
+  def set_job_expert(element)
+    case element.job_expert
+    when 1
+      job_expert = "fas fa-chess-pawn"
+    when 2
+      job_expert = "fas fa-chess-knight"
+    when 3
+      job_expert = "fas fa-chess-bishop"
+    when 4
+      job_expert = "fas fa-chess-rook"
+    when 5
+      job_expert = "fas fa-chess-queen"
+    end #end of case
+  end # end of def
+end # end of JobPost
